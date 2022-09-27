47e2b417508139d23ddfb8d09fe816c97a7d12c4
Andryss <andr.krivosheev@gmail.com>
2022-09-27 18:13:17 +0300

    Add problem id field (split problems into homeworks)

--- src/main/java/components/Problem.kt
+++ src/main/java/components/Problem.kt
@@ -1,6 +1,7 @@
 package components
 
 interface Problem {
+    val id: Int // homework id (1,2,3...)
     val label: String
     val svgPath: String // some picture on canvas 200x200
     val text: String
